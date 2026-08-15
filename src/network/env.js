2ace1450488a7d9e9fae185931ff04d68844a3fd
COURTESYCOIL <forbusiness6886543@gmail.com>
2026-08-15 18:19:59 +0300

    Automated Vercel Sync - Active Tunnel: https://champions-convergence-homeland-infants.trycloudflare.com

--- src/network/env.js
+++ src/network/env.js
@@ -1,1 +1,1 @@
-export const ACTIVE_TUNNEL_URL = 'https://vortex3d-live-8zx3y.trycloudflare.com';
+export const ACTIVE_TUNNEL_URL = 'https://champions-convergence-homeland-infants.trycloudflare.com';
