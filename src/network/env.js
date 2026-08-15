a7028f1dfa7ecef536df691cf03b9c60b32850ce
COURTESYCOIL <forbusiness6886543@gmail.com>
2026-08-15 22:17:10 +0300

    Automated Vercel Sync - Active Tunnel: https://receives-nutrition-attacked-allocated.trycloudflare.com

--- src/network/env.js
+++ src/network/env.js
@@ -1,1 +1,1 @@
-export const ACTIVE_TUNNEL_URL = 'https://vortex3d-live-wpx51.trycloudflare.com';
+export const ACTIVE_TUNNEL_URL = 'https://receives-nutrition-attacked-allocated.trycloudflare.com';
