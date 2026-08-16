79c9da4f62fca069ae92a9e6e1feef650c86d2cd
COURTESYCOIL <forbusiness6886543@gmail.com>
2026-08-16 14:11:57 +0300

    Automated Vercel Sync - Active Tunnel: https://sentence-citysearch-epa-instructional.trycloudflare.com

--- src/network/env.js
+++ src/network/env.js
@@ -1,1 +1,1 @@
-export const ACTIVE_TUNNEL_URL = 'https://vortex3d-live-n0x98.trycloudflare.com';
+export const ACTIVE_TUNNEL_URL = 'https://sentence-citysearch-epa-instructional.trycloudflare.com';
